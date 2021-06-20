36bf7fef3c9abf2f2735312843c2e91b1a047472
Wouter Mostard <mostard@dataprovider.com>
2021-06-20 22:28:46 +0200

    add GRU and MLP layers

--- coherence.py
+++ coherence.py
@@ -4,7 +4,7 @@ import torch
 
 
 class CNN(nn.Module):
-    def __init__(self, vocab_size=10000, embedding_dim=64, n_filters=[128, 256, 512], filter_sizes=[3, 3, 3], pad_idx=0):
+    def __init__(self, vocab_size=10000, embedding_dim=64, n_filters=[128, 256, 256], filter_sizes=[3, 5, 7], pad_idx=0):
         super().__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
@@ -21,23 +21,49 @@ class CNN(nn.Module):
                                 out_channels=n_filters[2],
                                 kernel_size=(filter_sizes[2], embedding_dim))
 
+        self.gru = nn.GRU(input_size=640, bidirectional=True, hidden_size=256, batch_first=False)
+
+        self.doc_mlp = nn.Sequential(
+            nn.Linear(512, 256),  # TODO, determine the right output shape
+            nn.Tanh()
+        )
+
     def forward(self, text):
         embedded = self.embedding(text)
         embedded = embedded.unsqueeze(1)
+        print(embedded.shape)
 
-        conved_0 = F.relu(self.conv_0(embedded).squeeze(3))
-        conved_1 = F.relu(self.conv_1(embedded).squeeze(3))
-        conved_2 = F.relu(self.conv_2(embedded).squeeze(3))
+        conved_0 = self.conv_0(embedded).squeeze(3)
+        print(conved_0.shape)
+        conved_1 = self.conv_1(embedded).squeeze(3)
+        conved_2 = self.conv_2(embedded).squeeze(3)
 
-        pooled_0 = F.avg_pool1d(conved_0, conved_0.shape[2]).squeeze(2)
-        pooled_1 = F.avg_pool1d(conved_1, conved_1.shape[2]).squeeze(2)
-        pooled_2 = F.avg_pool1d(conved_2, conved_2.shape[2]).squeeze(2)
+        pooled_0 = F.max_pool1d(conved_0, conved_0.shape[2]).squeeze(2)
+        print(pooled_0.shape)
+        pooled_1 = F.max_pool1d(conved_1, conved_1.shape[2]).squeeze(2)
+        pooled_2 = F.max_pool1d(conved_2, conved_2.shape[2]).squeeze(2)
 
         cat = torch.cat((pooled_0, pooled_1, pooled_2), dim=1)
+        print(cat.shape)
 
-        # TODO, create Bi-GRU to get the sentence representations
+        x = cat.mean(dim=0, keepdims=True)
 
-        return cat
+        x = torch.unsqueeze(torch.vstack([x, x, x, x]), 0) # TODO, so now we just have four of the same sentences in 1 document
+        print(x.shape)
+        output, hidden = self.gru(x)
+        print(hidden.shape)
+
+        h = torch.cat([hidden[0], hidden[1]], dim=1)
+
+        print(h.shape)
+
+        mean_sent = h.mean(dim=0, keepdims=True)
+        print(mean_sent.shape)
+
+        d = self.doc_mlp(mean_sent)
+        print(d.shape)
+
+        return x
 
 
 embeddings = torch.randint(low=1, high=400, size=(10, 50)).long()
